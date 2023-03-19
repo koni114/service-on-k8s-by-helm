5d8c8e58d56f15d92535cd578c96895d7e8d29e4
Jaehun Heo <koni114@naver.com>
2023-03-19 13:59:48 +0900

    add airflow chart

---
+++ airflow/install-airflow.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+RELEASE_NAME=airflow
+NAMESPACE_NAME=airflow
+AIRFLOW_TGZ=airflow-14.0.16.tgz
+
+# k8s namespace mlflow 확인 및 등록
+HAS_NS=$(kubectl get namespaces $NAMESPACE_NAME > /dev/null 2>&1 && echo true || echo false)
+
+if [ $HAS_NS != "true" ]; then
+    kubectl create namespace $NAMESPACE_NAME
+fi
+
+# minio 설치
+helm upgrade --install -f values-airflow.yaml airflow ./$AIRFLOW_TGZ --namespace $NAME_SPACE
